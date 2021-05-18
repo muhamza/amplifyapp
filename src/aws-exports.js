29705b4ab2b7a83344da7a443b086956dcf49a69
Muhammad Hamza <hamzashahid9@gmail.com>
2021-05-18 15:03:08 +0500

    api created

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,13 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_8UYMLVQ6g",
     "aws_user_pools_web_client_id": "3s1t86lckteq5ebf5lkt6c5o13",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://ljedfx2ytzcpdapqqqnnylra7y.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-xmlscx5g2fapzgq6pldlmwli64",
+    "aws_user_files_s3_bucket": "myimages60649-staging",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
